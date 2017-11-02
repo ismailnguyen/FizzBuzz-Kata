ab6df9d06e47c20c813576c0c62dccaa7dccf855
Ismaïl <nguyen.ismail@gmail.com>
2017-11-02 10:43:59 +0100

    Initialize empty project

---
+++ FizzBuzz-Kata.Tests/FizzBuzzTest.cs
@@ -0,0 +1,14 @@
+﻿using NFluent;
+using NUnit.Framework;
+
+namespace FizzBuzz_Kata.Tests
+{
+    public class FizzBuzzTest
+    {
+        [Test]
+        public void TODO_Rename()
+        {
+            Check.That(true).IsTrue();
+        }
+    }
+}
