9522b87e4302e8de06bfd6c1afbb51546e632221
Ismaïl <nguyen.ismail@gmail.com>
2017-11-02 11:36:43 +0100

    Refactor tests

--- FizzBuzz-Kata.Tests/FizzBuzzTest.cs
+++ FizzBuzz-Kata.Tests/FizzBuzzTest.cs
@@ -13,14 +13,16 @@ namespace FizzBuzz_Kata.Tests
             fizzBuzz = new FizzBuzz();
         }
 
-        [Test]
-        public void Should_Return_()
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(4)]
+        public void Should_Return_Same_Input(int input)
         {
-            int input = 1;
-
+            // WHEN
             string output = fizzBuzz.Compute(input);
 
-            Check.That(output).IsEqualTo("1");
+            // THEN
+            Check.That(output).IsEqualTo(input.ToString());
         }
 
         [TestCase(3)]
@@ -28,8 +30,10 @@ namespace FizzBuzz_Kata.Tests
         [TestCase(9)]
         public void Should_Return_Fizz_When_Input_Is_Multiple_Of_3(int input)
         {
+            // WHEN
             string output = fizzBuzz.Compute(input);
 
+            // THEN
             Check.That(output).IsEqualTo("Fizz");
         }
 
@@ -38,8 +42,10 @@ namespace FizzBuzz_Kata.Tests
         [TestCase(50)]
         public void Should_Return_Buzz_When_Input_Is_Multiple_Of_5(int input)
         {
+            // WHEN
             string output = fizzBuzz.Compute(input);
 
+            // THEN
             Check.That(output).IsEqualTo("Buzz");
         }
 
@@ -47,8 +53,10 @@ namespace FizzBuzz_Kata.Tests
         [TestCase(30)]
         public void Should_Return_FizzBuzz_When_Input_Is_Multiple_Of_3_And_Of_5(int input)
         {
+            // WHEN
             string output = fizzBuzz.Compute(input);
 
+            // THEN
             Check.That(output).IsEqualTo("FizzBuzz");
         }
     }
